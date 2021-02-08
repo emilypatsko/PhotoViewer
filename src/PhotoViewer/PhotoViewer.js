b32eef4ce31cb145ff933bf36a5fb89472384d0b
Emily Patsko <Emily.Patsko@softwire.com>
2021-02-08 11:31:13 +0000

    Created new directory and component for the large photo viewer.

---
+++ src/PhotoViewer/PhotoViewer.js
@@ -0,0 +1,11 @@
+import React from "react"; // import React for access to JSX
+
+function PhotoViewer() {
+    return (
+        <div>
+            Hello World!
+        </div>
+    );
+}
+
+export {PhotoViewer};
