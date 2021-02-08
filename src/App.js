1b8aab76c3281e58dfd088da6614d41f87a14298
Emily Patsko <Emily.Patsko@softwire.com>
2021-02-08 16:35:32 +0000

    Lifted up the state from the Image Selector into the app so that both components could use it.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 import {PhotoViewer} from './PhotoViewer/PhotoViewer.js';
 import {ImageSelector} from './ImageSelector/ImageSelector.js';
 
 function App() {
+
+  const [currentImg, setCurrentImg] = useState(0);
+  const urls = getImageUrls();
+
   return (
     <div>
       <h1>React Photo Viewer</h1>
-      <PhotoViewer src={"https://picsum.photos/id/1037/600/400"}/>
-      <ImageSelector/>
+      <PhotoViewer src = {urls[currentImg]}/>
+      <ImageSelector urls = {urls} currentImg = {currentImg} setCurrentImg = {setCurrentImg}/>
     </div>
   );
+}
+
+function getImageUrls() {
+  const images = [37, 47, 56, 25, 57, 32, 39, 18, 45, 36, 15, 23];
+  const urls = [];
+
+  for (let i = 0; i < images.length; i++) {
+      urls.push(`https://picsum.photos/id/10${images[i].toString()}/600/400`);
+  }
+
+  return urls;
 }
 
 export default App;
--- src/ImageSelector/ImageSelector.js
+++ src/ImageSelector/ImageSelector.js
@@ -1,37 +1,21 @@
 import React, { useState } from "react";
 import './ImageSelector.css';
 
-function ImageSelector() {
+function ImageSelector(props) {
 
-    // Declare a new state variable, call it currentImg
-    // Here the state number is the element of the images array currently displayed
-    const [currentImg, setCurrentImg] = useState(0);
-
-    const urls = getImageUrls();
     return (
         <div className = "image-selector-container">
-            <p>Selected image: <a href={urls[currentImg]}>{urls[currentImg]}</a></p>
+            <p>Selected image: <a href={props.urls[props.currentImg]}>{props.urls[props.currentImg]}</a></p>
             <div className = "image-selector">                 
-                {urls.map(function(url) {                    
-                    return <img src = {url} className = {`${currentImg==urls.indexOf(url) ? "selected-img" : ""}`} onClick = {() => {
-                        setCurrentImg(urls.indexOf(url))
-                    }
-                    }/>;
+                {props.urls.map(function(url) {
+                    return <img src = {url} 
+                                className = {`${props.currentImg==props.urls.indexOf(url) ? "selected-img" : ""}`}
+                                onClick = {() => props.setCurrentImg(props.urls.indexOf(url))}
+                            />                    
                 })}         
             </div>
         </div>
     );
-}
-
-function getImageUrls() {
-    const images = [37, 47, 56, 25, 57, 32, 39, 18, 45, 36, 15, 23];
-    const urls = [];
-
-    for (let i = 0; i < images.length; i++) {
-        urls.push(`https://picsum.photos/id/10${images[i].toString()}/300/200`);
-    }
-
-    return urls;
 }
 
 export {ImageSelector};
